9135718516bd2f6773f3f8807dc292bc33704790
Sebastian Shanus <sedward@berkeley.edu>
2017-01-07 18:38:55 -0800

    fix bug

--- index.js
+++ index.js
@@ -10,20 +10,22 @@ var YC_URL = "https://news.ycombinator.com/rss"
 
 app.get('/rss', function(req, res) {
 	feed(YC_URL, function(err, articles) {
-		if (err) throw err;
-		var feed = new RSS();
-		for (i in articles) {
-			c = articles[i];
-			feed.item(
-			{
-				title: c['title'],
-				feed_url: c['feed']['link'],
-				site_url: c['feed']['source']
-			});
+		if (err) {
+			res.send(err);
+		} else {
+			var feed = new RSS();
+			for (i in articles) {
+				c = articles[i];
+				feed.item(
+				{
+					title: c['title'],
+					feed_url: c['feed']['link'],
+					site_url: c['feed']['source']
+				});
+			}
+			var xml = feed.xml();
+			res.send(xml);
 		}
-		var xml = feed.xml();
-		console.log(xml);
-		res.send(xml);
 	});
 });
 
